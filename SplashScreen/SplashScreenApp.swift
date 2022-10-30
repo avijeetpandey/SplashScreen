d1d5c14cdab6e1e8b1bbd23057c705a9a68c5582
Avijeet Pandey <avijeetpandey25@gmail.com>
2022-10-30 10:36:57 +0530

    Initial Commit

---
+++ SplashScreen/SplashScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplashScreenApp.swift
+//  SplashScreen
+//
+//  Created by Avijeet Pandey on 30/10/22.
+//
+
+import SwiftUI
+
+@main
+struct SplashScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
